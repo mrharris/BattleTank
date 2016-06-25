8a2204962dcf29e1ba121bc813e9cd1618a559f1
Luke Harris <lukeha@gmail.com>
2016-06-25 17:40:51 +0100

    AI tanks find player tank

--- BattleTank/Source/BattleTank/TankAIController.cpp
+++ BattleTank/Source/BattleTank/TankAIController.cpp
@@ -10,15 +10,15 @@ void ATankAIController::BeginPlay()
 
 	UE_LOG(LogTemp, Warning, TEXT(""))
 
-	auto ControlledTank = GetControlledTank();
-	if (!ControlledTank)
+	auto PlayerTank = GetPlayerTank();
+	if (!PlayerTank)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("No AI Tank possessed"));
+		UE_LOG(LogTemp, Warning, TEXT("AI cant find player tank"));
 		return;
 	}
 	else
 	{
-		UE_LOG(LogTemp, Warning, TEXT("AI Tank is %s"), *(ControlledTank->GetName()));
+		UE_LOG(LogTemp, Warning, TEXT("AI found player %s"), *(PlayerTank->GetName()));
 	}
 }
 
@@ -26,5 +26,12 @@ void ATankAIController::BeginPlay()
 ATank* ATankAIController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (!PlayerPawn) { return nullptr; }
+	return Cast<ATank>(PlayerPawn);
 }
 

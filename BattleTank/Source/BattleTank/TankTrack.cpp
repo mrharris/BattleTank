353f7155fdc2f8f01c516bd8a4ec33e2c5dfcfe7
Luke Harris <lukeha@gmail.com>
2016-08-20 19:42:31 +0100

    Apply force from Tracks to Tank

--- BattleTank/Source/BattleTank/TankTrack.cpp
+++ BattleTank/Source/BattleTank/TankTrack.cpp
@@ -11,4 +11,8 @@ void UTankTrack::SetThrottle(float Throttle)
 	UE_LOG(LogTemp, Warning, TEXT("Track %s throttle is %f"), *GetName(), Throttle);
 
 	// TODO clamp throttle value
+	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	auto ForceLocation = GetComponentLocation();
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent()); // get Tank static mesh component
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
 }
--- BattleTank/Source/BattleTank/TankTrack.h
+++ BattleTank/Source/BattleTank/TankTrack.h
@@ -19,5 +19,8 @@ public:
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void SetThrottle(float Throttle);
 	
+	// Max force per track in newtons
+	UPROPERTY(EditDefaultsOnly)
+	float TrackMaxDrivingForce = 400000; // 40,000kg tank @ 10m/s/s
 	
 };
